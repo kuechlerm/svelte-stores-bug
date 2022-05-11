7af4c2a9f835614d1c2d003cb663577503dc585e
Martin Küchler <kuechlerm@gmail.com>
2022-05-11 20:26:40 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { asyncReadable } from '@square/svelte-store';
+  const ar = asyncReadable(null, () => Promise.resolve());
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
